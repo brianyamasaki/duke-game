25e23af090d9c6c5c38f73b5b171f0f591380928
Brian Yamasaki <brian.yamasaki@gmail.com>
2018-08-31 15:07:55 -0700

    make command polygons larger

--- src/components/game/shapes/commandPolygons.js
+++ src/components/game/shapes/commandPolygons.js
@@ -1,11 +1,11 @@
 export const commandPolyTop = [
   { x: -50, y: -50 },
-  { x: -5, y: -50 },
-  { x: -50, y: -5 }
+  { x: 0, y: -50 },
+  { x: -50, y: 0 }
 ];
 
 export const commandPolyBottom = [
-  { x: 50, y: 5 },
+  { x: 50, y: 0 },
   { x: 50, y: 50 },
-  { x: 5, y: 50 }
+  { x: 0, y: 50 }
 ];
