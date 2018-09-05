9959431dadfaaab7b5ecc5010c651b2f47f05a27
Brian Yamasaki <brian.yamasaki@gmail.com>
2018-09-05 15:53:45 -0700

    Improved about page as well as README

--- README.md
+++ README.md
@@ -16,10 +16,18 @@ yarn
 yarn start
 ```
 
-This boilerplate is built using [create-react-app](https://github.com/facebook/create-react-app) so you will want to read the User Guide for more goodies.
+This boilerplate is built using [create-react-app](https://github.com/facebook/create-react-app) so you will want to read their User Guide for more goodies.
 
-## To Do
+## Project Status
 
-* Allow selection of a tile on the board, highlight the possible moves, move the tile and/or capture an opposing tile.
-* Create an object that manages the bag of tiles. Allow receiving a random tile, choose between 3 tiles, etc. Also manage captured tiles.
-* Create second player and allow them to play.
+The project is basically playable, where most basic tiles are created and move rules are supported. Currently it's only playing inside a single browser and players must click the End of Turn to allow the other player to move. 
+
+## Not supported yet
+We don't support the Mountain or Flag tiles or rules for those pieces. In addition, tile support will get better over time.
+
+## Long term goals
+
+* Support playing 1 on 1 play with player matching services on the server
+* Dynamic creation of tiles using existing rules.
+* Touch screen support
+* improved mobile phone support
--- src/containers/about/index.js
+++ src/containers/about/index.js
@@ -1,25 +1,47 @@
 import React from 'react'
-import { Grid } from 'react-bootstrap'
+import { Grid, Row, Col } from 'react-bootstrap'
+import packageJson from '../../../package.json';
 
 const details = [
-  'Now both dark and light tiles can capture tiles!',
-  'Supporting moves blocked by tiles'
+  'Command moves are now supported',
+  'All tiles are drawn using SVG, meaning they literally reflect the movement rules'
 ];
 const todos = [
 ];
 const About = () => (
   <Grid>
     <h1 className="text-center">About</h1>
-    <h2>The Project</h2>
-    <p>This is an open source project written in React. The code is available on <a href="https://github.com/brianyamasaki/duke-game">https://github.com/brianyamasaki/duke-game</a></p>
-    <h2>New Features</h2>
-    <ul>
-      {details.map((detail, i) => (<li key={i}>{detail}</li>))}
-    </ul>
-    <h2>To Do</h2>
-    <ul>
-      {todos.map((todo, i) => (<li key={i}>{todo}</li>))}
-    </ul>
+    <Row>
+      <Col sm={12} md={6} lg={7}>
+        <h2>The Project</h2>
+        <p>
+          This game is a work in progress. Currently both players play on the same browser and must click the End of Turn button to let the other player play,
+          mostly because we haven't implemented Undo yet. We also don't save the game when you navigate away from the page. All this in an effort to get the game
+          playable as soon as possible. We will eventually create a server and a player matching service to allow players to play
+          over the Internet.
+        </p>
+        <p>
+          It's currently designed just using a mouse and simple clicks, no drag and drop support or special
+          touchscreen support yet. 
+        </p>
+        <p>
+          This is an open source project written in React. The code is available on <a href="https://github.com/brianyamasaki/duke-game">https://github.com/brianyamasaki/duke-game</a>. 
+        </p>
+        <p>
+          Version {packageJson.version} &mdash; Copyright 2018 Yamasaki Design.
+        </p>
+      </Col>
+      <Col sm={12} md={6} lg={5}>
+        <h2>New Features</h2>
+        <ul>
+          {details.map((detail, i) => (<li key={i}>{detail}</li>))}
+        </ul>
+        <h2>To Do</h2>
+        <ul>
+          {todos.map((todo, i) => (<li key={i}>{todo}</li>))}
+        </ul>
+      </Col>
+    </Row>
   </Grid>
 )
 
